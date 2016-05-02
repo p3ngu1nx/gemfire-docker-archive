d89dbe7693ac697356ddd3be5b264e4a3e6c9384
boriza <mail@boriza.com>
2016-05-02 14:23:20 +1000

    working quickstart

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,9 +1,12 @@
-cd /bk/Dev/Docker/gemfire/docker-gemfire/locator
-docker build -t pivotal/gemfire-locator .
-docker run -p 41111:41111 -p 1099:1099 --name locator -dti --add-host locatorhost:172.17.0.2 --add-host dockerhost:192.168.99.100 -d pivotal/gemfire-locator 
-cd /bk/Dev/Docker/gemfire/docker-gemfire/server
-docker build -t pivotal/gemfire-server .
-docker run -p 40404:40404 --name server -dti --add-host dockerhost:192.168.99.100 --add-host locatorhost:172.17.0.2 -d pivotal/gemfire-server 
-cd /bk/Dev/Docker/gemfire/docker-gemfire/client
-docker build -t pivotal/gemfire-client .
-docker run --name client -dti --add-host locatorhost:172.17.0.2 --add-host dockerhost:192.168.99.100 -d pivotal/gemfire-client
+cd locator
+docker build -t boriza/gemfire-locator .
+docker run -p 41111:41111 -p 1099:1099 --name locator -dti --add-host locatorhost:172.17.0.2 --add-host dockerhost:192.168.99.100 -d boriza/gemfire-locator 
+cd..
+cd server
+docker build -t boriza/gemfire-server .
+docker run -p 40404:40404 --name server -dti --add-host dockerhost:192.168.99.100 --add-host locatorhost:172.17.0.2 -d boriza/gemfire-server 
+cd ..
+cd client
+docker build -t boriza/gemfire-client .
+docker run --name client -dti --add-host locatorhost:172.17.0.2 --add-host dockerhost:192.168.99.100 -d boriza/gemfire-client
+cd..
