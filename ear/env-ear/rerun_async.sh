9aa06cffedb9c9c382b3a140183576c6e048401a
boriza <boris@Automata-3.local>
2016-05-31 16:07:04 +1000

    print datastore

--- ear/env-ear/rerun_async.sh
+++ ear/env-ear/rerun_async.sh
@@ -4,4 +4,4 @@
 (sleep 30; echo "***************  CONSUMER **************"; docker-compose up consumer ) &
 (sleep 35; echo "***************  WORKER **************"; docker-compose up worker ) &
 (sleep 40; echo "***************  SERVER **************"; docker exec -it envear_locator_1 gfsh -e "connect" -e "stop server --name=server1" -e "start server --name=server1" ;) &
-(sleep 50; echo "***************  DONE **************"; docker logs envear_server_1; docker exec -it envear_server_1 cat /Pivotal_GemFire/SampleCode/quickstart/server1/server1.log; docker-compose down )
+(sleep 50; echo "***************  DONE **************"; docker exec -it envear_server_1 cat server1/persistData1/BACKUPds1_1.crf; docker logs envear_server_1; docker exec -it envear_server_1 cat /Pivotal_GemFire/SampleCode/quickstart/server1/server1.log; docker-compose down ;)
